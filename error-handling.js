ba49bedfa9be514e68d34c2ccc84a93e10ee5bd9
Andrew <andrew.lizon12@gmail.com>
2024-05-01 12:16:13 -0700

    Completed Step 2

--- error-handling.js
+++ error-handling.js
@@ -8,16 +8,29 @@ function sum(array) {
 }
 
 // The try .. catch block allows us to test a statement that may print an error without the stopping execution of all code.
-try {let res = sum(null);
-console.log(res);}
-catch {
-  console.error("Error occurred")
-}
+// try {let res = sum(null);
+// console.log(res);}
+// catch {
+//   console.error("Error occurred")
+// }
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
+function sayName(name) {
+if (typeof name !== "string") {
+  throw new Error ("Invalid name! Must be a string!")
+} 
+return name
+}
+// // Do not have to print to terminal to see the result of the invocations
+// try {console.log(sayName("Alex"));
+// sayName(1);
+// } 
+// // can pass an error parameter to create a message only if the function has an error message
+// catch (error) {
+//   console.error(error.name + ": " + error.message)
+// }
+
 
 
 // 3.
